57eb6ca29b93e010daa304fa6e56a79ade36d26c
glejeune <gregoire.lejeune@free.fr>
2016-06-28 12:28:52 +0200

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bristow.Mixfile do
   def project do
     [
       app: :bristow,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
